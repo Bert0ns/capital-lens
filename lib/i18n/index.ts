4cf27151ba19eb73741a822b1e33b669e6222471
bert0ns <davidebe03@gmail.com>
2026-06-17 01:18:45 +0200

    refactor

--- lib/i18n/index.ts
+++ lib/i18n/index.ts
@@ -1,91 +1,91 @@
 // Auto-generated i18n barrel file
 
-import en_navbar from './locales/en/navbar.json';
-import en_languageSwitcher from './locales/en/languageSwitcher.json';
-import en_sectors from './locales/en/sectors.json';
-import en_countries from './locales/en/countries.json';
-import en_etfProperties from './locales/en/etfProperties.json';
-import en_notifications from './locales/en/notifications.json';
-import en_dashboard from './locales/en/dashboard.json';
-import en_overviewTab from './locales/en/overviewTab.json';
-import en_etfForm from './locales/en/etfForm.json';
-import en_fundDetailsTab from './locales/en/fundDetailsTab.json';
-import en_riskAnalysisTab from './locales/en/riskAnalysisTab.json';
-import en_deepDiveTab from './locales/en/deepDiveTab.json';
-import en_savingsPlan from './locales/en/savingsPlan.json';
-import en_landing from './locales/en/landing.json';
-import en_about from './locales/en/about.json';
-import en_analyzer from './locales/en/analyzer.json';
-import en_portfolioSliders from './locales/en/portfolioSliders.json';
-import en_charts from './locales/en/charts.json';
-import en_diversification from './locales/en/diversification.json';
-import en_threeDVisuals from './locales/en/threeDVisuals.json';
+import en_landing from './locales/en/pages/landing.json';
+import en_about from './locales/en/pages/about.json';
+import en_analyzer from './locales/en/pages/analyzer/main.json';
+import en_navbar from './locales/en/components/common/navbar.json';
+import en_languageSwitcher from './locales/en/components/common/languageSwitcher.json';
+import en_notifications from './locales/en/components/common/notifications.json';
+import en_etfForm from './locales/en/pages/analyzer/components/etfForm.json';
+import en_portfolioSliders from './locales/en/pages/analyzer/components/portfolioSliders.json';
+import en_savingsPlan from './locales/en/pages/analyzer/components/savingsPlan.json';
+import en_dashboard from './locales/en/pages/analyzer/dashboard/main.json';
+import en_overviewTab from './locales/en/pages/analyzer/dashboard/tabs/overviewTab.json';
+import en_fundDetailsTab from './locales/en/pages/analyzer/dashboard/tabs/fundDetailsTab.json';
+import en_riskAnalysisTab from './locales/en/pages/analyzer/dashboard/tabs/riskAnalysisTab.json';
+import en_deepDiveTab from './locales/en/pages/analyzer/dashboard/tabs/deepDiveTab.json';
+import en_threeDVisuals from './locales/en/pages/analyzer/dashboard/tabs/threeDVisuals.json';
+import en_charts from './locales/en/pages/analyzer/dashboard/widgets/charts.json';
+import en_diversification from './locales/en/pages/analyzer/dashboard/widgets/diversification.json';
+import en_sectors from './locales/en/data/sectors.json';
+import en_countries from './locales/en/data/countries.json';
+import en_etfProperties from './locales/en/data/etfProperties.json';
 
-import it_navbar from './locales/it/navbar.json';
-import it_languageSwitcher from './locales/it/languageSwitcher.json';
-import it_sectors from './locales/it/sectors.json';
-import it_countries from './locales/it/countries.json';
-import it_etfProperties from './locales/it/etfProperties.json';
-import it_notifications from './locales/it/notifications.json';
-import it_dashboard from './locales/it/dashboard.json';
-import it_overviewTab from './locales/it/overviewTab.json';
-import it_etfForm from './locales/it/etfForm.json';
-import it_fundDetailsTab from './locales/it/fundDetailsTab.json';
-import it_riskAnalysisTab from './locales/it/riskAnalysisTab.json';
-import it_deepDiveTab from './locales/it/deepDiveTab.json';
-import it_savingsPlan from './locales/it/savingsPlan.json';
-import it_landing from './locales/it/landing.json';
-import it_about from './locales/it/about.json';
-import it_analyzer from './locales/it/analyzer.json';
-import it_portfolioSliders from './locales/it/portfolioSliders.json';
-import it_charts from './locales/it/charts.json';
-import it_diversification from './locales/it/diversification.json';
-import it_threeDVisuals from './locales/it/threeDVisuals.json';
+import it_landing from './locales/it/pages/landing.json';
+import it_about from './locales/it/pages/about.json';
+import it_analyzer from './locales/it/pages/analyzer/main.json';
+import it_navbar from './locales/it/components/common/navbar.json';
+import it_languageSwitcher from './locales/it/components/common/languageSwitcher.json';
+import it_notifications from './locales/it/components/common/notifications.json';
+import it_etfForm from './locales/it/pages/analyzer/components/etfForm.json';
+import it_portfolioSliders from './locales/it/pages/analyzer/components/portfolioSliders.json';
+import it_savingsPlan from './locales/it/pages/analyzer/components/savingsPlan.json';
+import it_dashboard from './locales/it/pages/analyzer/dashboard/main.json';
+import it_overviewTab from './locales/it/pages/analyzer/dashboard/tabs/overviewTab.json';
+import it_fundDetailsTab from './locales/it/pages/analyzer/dashboard/tabs/fundDetailsTab.json';
+import it_riskAnalysisTab from './locales/it/pages/analyzer/dashboard/tabs/riskAnalysisTab.json';
+import it_deepDiveTab from './locales/it/pages/analyzer/dashboard/tabs/deepDiveTab.json';
+import it_threeDVisuals from './locales/it/pages/analyzer/dashboard/tabs/threeDVisuals.json';
+import it_charts from './locales/it/pages/analyzer/dashboard/widgets/charts.json';
+import it_diversification from './locales/it/pages/analyzer/dashboard/widgets/diversification.json';
+import it_sectors from './locales/it/data/sectors.json';
+import it_countries from './locales/it/data/countries.json';
+import it_etfProperties from './locales/it/data/etfProperties.json';
 
 export const dictionaries = {
   en: {
-    navbar: en_navbar,
-    languageSwitcher: en_languageSwitcher,
-    sectors: en_sectors,
-    countries: en_countries,
-    etfProperties: en_etfProperties,
-    notifications: en_notifications,
-    dashboard: en_dashboard,
-    overviewTab: en_overviewTab,
-    etfForm: en_etfForm,
-    fundDetailsTab: en_fundDetailsTab,
-    riskAnalysisTab: en_riskAnalysisTab,
-    deepDiveTab: en_deepDiveTab,
-    savingsPlan: en_savingsPlan,
     landing: en_landing,
     about: en_about,
     analyzer: en_analyzer,
+    navbar: en_navbar,
+    languageSwitcher: en_languageSwitcher,
+    notifications: en_notifications,
+    etfForm: en_etfForm,
     portfolioSliders: en_portfolioSliders,
+    savingsPlan: en_savingsPlan,
+    dashboard: en_dashboard,
+    overviewTab: en_overviewTab,
+    fundDetailsTab: en_fundDetailsTab,
+    riskAnalysisTab: en_riskAnalysisTab,
+    deepDiveTab: en_deepDiveTab,
+    threeDVisuals: en_threeDVisuals,
     charts: en_charts,
     diversification: en_diversification,
-    threeDVisuals: en_threeDVisuals,
+    sectors: en_sectors,
+    countries: en_countries,
+    etfProperties: en_etfProperties,
   },
   it: {
-    navbar: it_navbar,
-    languageSwitcher: it_languageSwitcher,
-    sectors: it_sectors,
-    countries: it_countries,
-    etfProperties: it_etfProperties,
-    notifications: it_notifications,
-    dashboard: it_dashboard,
-    overviewTab: it_overviewTab,
-    etfForm: it_etfForm,
-    fundDetailsTab: it_fundDetailsTab,
-    riskAnalysisTab: it_riskAnalysisTab,
-    deepDiveTab: it_deepDiveTab,
-    savingsPlan: it_savingsPlan,
     landing: it_landing,
     about: it_about,
     analyzer: it_analyzer,
+    navbar: it_navbar,
+    languageSwitcher: it_languageSwitcher,
+    notifications: it_notifications,
+    etfForm: it_etfForm,
     portfolioSliders: it_portfolioSliders,
+    savingsPlan: it_savingsPlan,
+    dashboard: it_dashboard,
+    overviewTab: it_overviewTab,
+    fundDetailsTab: it_fundDetailsTab,
+    riskAnalysisTab: it_riskAnalysisTab,
+    deepDiveTab: it_deepDiveTab,
+    threeDVisuals: it_threeDVisuals,
     charts: it_charts,
     diversification: it_diversification,
-    threeDVisuals: it_threeDVisuals,
+    sectors: it_sectors,
+    countries: it_countries,
+    etfProperties: it_etfProperties,
   },
 };
 
