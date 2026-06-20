8d320402a494640de8926b18d741947f7de61d0f
bert0ns <davidebe03@gmail.com>
2026-06-20 20:07:01 +0200

    fix: icon not showing on desktop install

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,9 +11,15 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#000000',
     icons: [
       {
-        src: '/icon.svg',
-        sizes: '192x192 512x512',
-        type: 'image/svg+xml',
+        src: '/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'any maskable',
+      },
+      {
+        src: '/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
         purpose: 'any maskable',
       },
     ],
